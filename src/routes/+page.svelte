8f5d004bb13b8fb3505efc09c86b406d619bdf4c
SambhavG <sambhavgupta159@gmail.com>
2024-08-11 23:07:12 -0700

    Make edit with testing enabled

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,14 @@
 </script>
 
 <div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
 
 <style lang="postcss">
 </style>
