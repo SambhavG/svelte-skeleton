8a9d44f26d964f15710f09ec6796ab207b9b7f14
SambhavG <sambhavgupta159@gmail.com>
2024-08-11 22:05:39 -0700

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	// import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="text-9xl font-bold flex justify-center h-screen bg-red-500">Svelte pages default</div>
+
+<style lang="postcss">
+</style>
